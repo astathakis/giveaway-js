a93db002e81abd14550e5ac90ec67c1356b5c568
tasos <tasos_stathakis@yahoo.com>
2022-06-24 17:08:39 +0300

    add date

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
 ];
 const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2022, 10, 24, 11, 30, 0);
+// let futureDate = new Date();
+// console.log(futureDate);
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const mins = futureDate.getMinutes();
+// const secs = futureDate.getSeconds();
+let month = futureDate.getMonth();
+// console.log(months[month]);
+month = months[month];
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${mins}am `;
